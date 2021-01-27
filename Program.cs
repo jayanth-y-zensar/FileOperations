099a6772e442ffa0f4b19309a2f404365d457ef7
jayanth_y <jayanth.yedureswaram@zensar.com>
2021-01-27 10:09:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperations
+{
+    class Program
+    {
+        static void WriteToFile()
+        {
+            Console.WriteLine("Write To File Method: ");
+            string inputData;
+            try
+            {
+                using (StreamWriter SW = new StreamWriter(@"C:\FileOne.txt"))
+                {
+                    while (!String.IsNullOrWhiteSpace(inputData = Console.ReadLine()))
+                    SW.WriteLine(inputData);
+                }
+            }
+            catch(Exception E)
+            {
+                Console.WriteLine(E.Message);
+            }
+        }
+
+        static void ReadFromFile()
+        {
+            Console.WriteLine("Read From File Method: ");
+            try
+            {
+                using (StreamReader SR = new StreamReader(@"C:\FileOne.txt"))
+                {
+                    Console.WriteLine(SR.ReadToEnd());
+                }
+            }
+            catch (Exception E)
+            {
+                Console.WriteLine(E.Message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            WriteToFile();
+            ReadFromFile();
+        }
+    }
+}
